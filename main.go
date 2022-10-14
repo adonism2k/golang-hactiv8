77cc6e671b2ce170a5a823d8cfb2026d99b26dea
adonisme <abdianrizky11@gmail.com>
2022-10-14 13:22:59 +0700

    Assignment 3 Done✅

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -22,9 +24,6 @@ type Data struct {
 var count int
 
 func main() {
-
-	WriteJSON()
-
 	r := gin.Default()
 	r.LoadHTMLGlob("./templates/*")
 
@@ -34,18 +33,51 @@ func main() {
 }
 
 func WriteHTML(c *gin.Context) {
-	data := gin.H{
-		"wind_status":  "Bahaya",
-		"water_status": "Bahaya",
+	WriteJSON()
+	file, err := ioutil.ReadFile("status.json")
+	if err != nil {
+		panic(err)
 	}
 
-	c.HTML(200, "index.html", data)
+	var data Status
+	err = json.Unmarshal(file, &data)
+	if err != nil {
+		panic(err)
+	}
+
+	var wind_status, water_status string
+
+	switch {
+	case data.Status.Water < 5:
+		water_status = "Aman" + strconv.Itoa(data.Status.Water)
+	case data.Status.Water > 6 && data.Status.Water < 8:
+		water_status = "Siaga" + strconv.Itoa(data.Status.Water)
+	case data.Status.Water > 8:
+		water_status = "Bahaya" + strconv.Itoa(data.Status.Water)
+	}
+
+	switch {
+	case data.Status.Wind < 6:
+		wind_status = "Aman" + strconv.Itoa(data.Status.Wind)
+	case data.Status.Wind > 7 && data.Status.Wind < 15:
+		wind_status = "Siaga" + strconv.Itoa(data.Status.Wind)
+	case data.Status.Wind > 15:
+		wind_status = "Bahaya" + strconv.Itoa(data.Status.Wind)
+	}
+
+	c.HTML(200, "index.tmpl", gin.H{
+		"wind":  wind_status,
+		"water": water_status,
+	})
 }
 
 func WriteJSON() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	wind := rand.Intn(100)
 	water := rand.Intn(100)
+
+	log.Println("Wind: ", wind)
+	log.Println("Water: ", water)
 
 	data := Status{
 		Data{
